b28ff9ff2446dd7d4eb5d1a283f023d08b0557ad
Manh <nvmanh@cmc.com.vn>
2020-08-01 20:02:40 +0700

    update

---
+++ Day22_OOP_PDO_And_MVC/Codes_demo/4_tinh_chat_oop.php
@@ -0,0 +1,9 @@
+<!--
+4_tinh_chat_oop.php
+- Tính trừu tượng: liên quan đến các class abstract
+- Tính đóng gói: liên quan đến phạm vi truy cập của OOP: private,
+protected, public
+- Tính kế thừa: liên quan đến từ khóa extends, thể hiện cho sự
+kế thừa
+- Tính đa hình: liên quan đến interface
+-->
